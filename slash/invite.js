af5c3340f3dd0b95616d975bf3d0ef0f6546c13d
SIDIUL AKBAR <siddimaster313@gmail.com>
2026-06-18 12:37:50 +0530

    invite command

--- slash/invite.js
+++ slash/invite.js
@@ -1,35 +1,30 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const config = require('../config.json');
 
-module.exports = {
-    name: 'invite',
-    description: '➕ Invite the bot to your server!',
-    run: async (client, interaction) => {
+module.exports.run = async (client, message, args) => {
     const row = new ActionRowBuilder()
     .addComponents(
         new ButtonBuilder()
         .setLabel(`Invite ${client.user.username}`)
         .setStyle(ButtonStyle.Link)
-        .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`),
+        .setURL(`https://discord.com/oauth2/authorize?client_id=1513767218821988492&permissions=70922291768432&integration_type=0&scope=bot`),
         new ButtonBuilder()
         .setLabel('Support Server')
         .setStyle(ButtonStyle.Link)
-        .setURL("https://discord.gg/ARu4hr6hJw"),
+        .setURL("https://discord.gg/JQbThGUns"),
     )
     let invite = new EmbedBuilder()
-      .setAuthor({ 
+     .setAuthor({ 
           name: `Invite ${client.user.username}`, 
           iconURL: client.user.displayAvatarURL() 
-      })    
+     })  
     .setTitle("Invite & Support Link!")
-    .setDescription(`Invite ${client.user} to your server today & enjoy seamless giveaways with advvanced features!`)
+    .setDescription(`Invite ${client.user} to your server today & enjoy seamless giveaways with advanced features!`)
     .setColor('#2F3136')
     .setTimestamp()
     .setFooter({
-        text: `Requested by ${interaction.user.username} | ` + config.copyright,
-        iconURL: interaction.user.displayAvatarURL()
-    })
-    
-    interaction.reply({ embeds: [invite], components: [row]});
-}
+      text: `Requested by ${message.author.username} | ` + config.copyright, 
+      iconURL: message.author.displayAvatarURL()
+    });
+    message.reply({ embeds: [invite], components: [row]});
 }
